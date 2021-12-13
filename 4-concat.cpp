82305eb0a19c81b932b56d8d49bce6ae800c333e
AvangardAA <70914823+AvangardAA@users.noreply.github.com>
2021-12-13 05:13:46 +0200

    Add files via upload

---
+++ 4-concat.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+void faster_out(vector<int>& vec)
+{
+    string s = "";
+
+    for (int i = 0; i < vec.size(); i++)
+    {
+        s += to_string(vec[i]) + " ";
+    }
+
+    cout << s;
+}
+
+int main()
+{
+    vector<int> first = { 0, 1, 1, 5, 2 };
+    vector<int> second = { 1, 2, 3, 4 }; // lets use our first vector after concat
+
+    for (int i = 0; i < second.size(); i++)
+    {
+        first.push_back(second[i]);
+    }
+
+    faster_out(first);
+}
